07e1a50af08f3d6e1b49dbf4919dfae73a19aaa3
Loic <talhouar.loic@gmail.com>
2015-01-31 13:11:56 -0500

    Revert "changed location of index"
    This reverts commit f5fb5795af8b6f4350bedeee58839ac5d0b8612f.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 5000
 
 app.get('/', function(req, res){
-    res.sendFile('http://loictalhouarne.com/socket.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
